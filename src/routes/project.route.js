430189a3b51c8a2dac7bc498cf4c1d299e1c58da
UsaidP <arkammulla@gmail.com>
2025-06-11 17:37:11 +0530

    update route add and test create route of project

--- src/routes/project.route.js
+++ src/routes/project.route.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { createProject } from "../controllers/project.controller.js";
+import {
+  createProject,
+  updateProject,
+} from "../controllers/project.controller.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { protect } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/create", protect, asyncHandler(createProject));
+router.post("/update", protect, asyncHandler(updateProject));
 export default router;
